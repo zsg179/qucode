a41649ad751284bafda0cf0a43f8b46a647b0181
zsg179 <18977330642@163.com>
2020-07-09 17:34:35 +0800

    Revert "可显示二维码"
    This reverts commit b9a4fe21

--- src/main/java/com/zhusg02/qucode/controller/QRCodeController.java
+++ src/main/java/com/zhusg02/qucode/controller/QRCodeController.java
@@ -6,26 +6,45 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-import com.zhusg02.qucode.utils.QRCodeUtil;
 import com.zhusg02.qucode.vo.RespTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.util.Map;
 
 @RestController
 public class QRCodeController {
 
-    private static final String QR_CODE_IMAGE_PATH = "E:/svn/repository/core-app/qrcode/src/assets/";
-
+    private static final String QR_CODE_IMAGE_PATH = "E:/qrcodes/";
     @CrossOrigin(allowCredentials = "true", allowedHeaders = "*", methods = { RequestMethod.POST, RequestMethod.GET,
             RequestMethod.HEAD, RequestMethod.PUT, RequestMethod.PATCH }, origins = "*")
     @PostMapping("/qrcode")
-    public RespTemplate getQRCode(@RequestBody Map map) {
+    public RespTemplate getQRCode(@RequestBody String businessNumber,@RequestBody String projectName) {
+        JSONObject jsonb = new JSONObject();
         RespTemplate res = new RespTemplate();
-        String businessNumber = (String) map.get("businessNumber");
+        System.out.println(businessNumber);
         String filePath = QR_CODE_IMAGE_PATH+businessNumber+".png";
+        System.out.println(filePath);
+        /*byte[] qrcode = null;
+        try {
+            qrcode = getQRCodeImage("https://mi.szpisp.cn/szsb/?salesmanCode=TKPC" + businessNumber, 360, 360);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+
+        return null;*/
 
         //本地生成图片
         try {
@@ -35,25 +54,9 @@ public class QRCodeController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        /*try {
-            QRCodeUtil.encode("https://localhost:8082/toView/?url=https://mi.szpisp.cn/szsb/?salesmanCode=TKPC" + businessNumber
-            , null,filePath,businessNumber+".jpg");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
         res.setCode("200");
         res.setMessage("success");
-        res.setData(businessNumber);
-        return res;
-    }
-
-    @CrossOrigin(allowCredentials = "true", allowedHeaders = "*", methods = { RequestMethod.POST, RequestMethod.GET,
-            RequestMethod.HEAD, RequestMethod.PUT, RequestMethod.PATCH }, origins = "*")
-    @PostMapping("/toView")
-    public RespTemplate toView(String url) {
-        System.out.println(url);
-        RespTemplate res = new RespTemplate();
+        res.setData(filePath);
         return res;
     }
 
@@ -67,5 +70,15 @@ public class QRCodeController {
         MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
 
     }
+
+   /* public static byte[] getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+        byte[] pngData = pngOutputStream.toByteArray();
+        return pngData;
+    }*/
 
 }
